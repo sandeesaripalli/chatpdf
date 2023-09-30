821f44a92d7dcb79c0fa767d0fed39570603a8b5
sandeepsaripalli <hawks900@gmail.com>
2023-09-29 22:49:05 -0400

    fixed go to chats button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import FileUpload from '@/components/FileUpload';
 import { Button } from '@/components/ui/button'
+import { db } from '@/lib/db';
+import { chats } from '@/lib/db/schema';
 import { UserButton, auth } from '@clerk/nextjs'
-import { LogIn } from 'lucide-react';
+import { eq } from 'drizzle-orm';
+import { ArrowRight, LogIn } from 'lucide-react';
 import Image from 'next/image'
 import Link from 'next/link';
 
 export default async function Home() {
   const {userId} = await auth();
   const isAuthenticated = !!userId
+
+  let firstChat;
+  if(userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId,  userId));
+    if(firstChat){
+      firstChat =firstChat[0]
+    }
+  }
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -18,7 +29,15 @@ export default async function Home() {
 
         </div>
         <div className='flex mt-2'>
-        {isAuthenticated && <Button>Go to Chats</Button>}
+        {isAuthenticated && firstChat && (
+              <>
+                <Link href={`/chat/${firstChat.id}`}>
+                  <Button>
+                    Go to Chats <ArrowRight className="ml-2" />
+                  </Button>
+                </Link>
+                </>
+        )}
         </div>
         <h3 className="text-2xl">Unlock the Power of Knowledge with Our Chat PDF Tool</h3>
         <p className="max-w-xl mt-1 text-md text-slate-600">Join the future of research and discovery. Experience the convenience, efficiency, and simplicity of our Chat PDF Tool today. Your quest for knowledge begins here.</p>
