3504d9d4ee367cbda6df947c1550adef70cce8ad
sandeepsaripalli <hawks900@gmail.com>
2023-10-10 22:18:20 -0400

    added the functionality to incorporate self hrendered pdf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          // fixes proxy-agent dependencies
+          net: false,
+          dns: false,
+          tls: false,
+          assert: false,
+          // fixes next-i18next dependencies
+          path: false,
+          fs: false,
+          // fixes mapbox dependencies
+          events: false,
+          // fixes sentry dependencies
+          process: false,
+        },
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,30 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './global.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Providers from '@/components/Providers'
-import { Toaster } from 'react-hot-toast'
- 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./global.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
+import { Toaster } from "react-hot-toast";
+import "simplebar-react/dist/simplebar.min.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ChatPdf',
-  
-}
+  title: "ChatPdf",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <Providers>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Toaster />
-    </html>
-    
-    </Providers>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+          <Toaster />
+        </html>
+      </Providers>
     </ClerkProvider>
-  )
+  );
 }
