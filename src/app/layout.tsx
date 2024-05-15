29a140ba290c558eabe357ffed402ba3202e828f
sandeesaripalli <hawks900@gmail.com>
2024-05-15 16:58:50 -0400

    added some changes related to microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import Providers from "@/components/Providers";
 import { Toaster } from "react-hot-toast";
 import "simplebar-react/dist/simplebar.min.css";
-import Script from "next/script";
+import Metrics from "./metrics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,14 +24,7 @@ export default function RootLayout({
         <html lang="en">
           <body className={inter.className}>
             {children}
-            <Script id="clarity-script" strategy="afterInteractive">
-              {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");`}
-            </Script>
+            <Metrics />
           </body>
           <Toaster />
         </html>
